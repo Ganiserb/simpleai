cd5eecf8467865ee78f00c26cee52c262ed3062d
ganiserb <g.gabriel.b@gmail.com>
2015-08-15 23:21:16 -0300

    included the graph in dot string format on the step response

--- simpleai/search/web_viewer_server.py
+++ simpleai/search/web_viewer_server.py
@@ -54,6 +54,7 @@ def run_server(viewer):
                     data = {}
                     data['stats'] = [{'name': stat.replace('_', ' '), 'value': value}
                                      for stat, value in viewer.stats.items()]
+                    data['graph_dot_string'] = viewer.graph_dot_string
 
                     for event in viewer.events[announced:news_limit]:
                         data['event'] = event.__dict__
